feb048b54f2b300be4f28b52da4510d657538f23
Tina Ostrander <tostrander@greenriver.edu>
2018-01-26 14:36:00 -0600

    added comments to Pet class

--- classes/pet.php
+++ classes/pet.php
@@ -4,42 +4,85 @@ class Pet
     private $_name;
     private $_color;
 
+    /**
+     * Construct a Pet object
+     *
+     * Optional longer, multi-line
+     * description with further details.
+     *
+     */
     function __construct()
     {
         $this->_name = $name;
         $this->_color = $color;
     }
 
+    /**
+     * Tell the pet to eat
+     *
+     */
     function eat()
     {
         echo $this->_name . " is Eating...<br>";
     }
 
+    /**
+     * Tell the pet to talk
+     *
+     */
     function talk()
     {
         echo $this->_name . " is Talking...<br>";
     }
 
+    /**
+     * Tell the pet to sleep
+     *
+     */
     function sleep()
     {
         echo $this->_name . " is Sleeping...<br>";
     }
 
+    /**
+     * Set the pet's name
+     *
+     * @param String $name The pet's name
+     *
+     */
     function setName($name)
     {
         $this->_name = $name;
     }
 
+    /**
+     * Get the pet's name
+     *
+     * @return String The pet's name
+     *
+     */
     function getName()
     {
         return $this->_name;
     }
 
+    /**
+     * Set the pet's color
+     *
+     * @param String $color The pet's color
+     *
+     */
     function setColor($color)
     {
         $this->_color = $color;
     }
 
+    /**
+     * Get the pet's color
+     *
+     * @return String The pet's color
+     *
+     */
     function getColor()
     {
         return $this->_color;
